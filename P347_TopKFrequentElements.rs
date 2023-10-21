9b2ff6753a5ace8571ae78f3301287c043459132
Ken Wong <kiuare310@gmail.com>
2023-10-21 16:52:08 +0800

    refactor(347): refactor still O(n)

--- P347_TopKFrequentElements.rs
+++ P347_TopKFrequentElements.rs
@@ -1,10 +1,33 @@
 impl Solution {
     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        // Solution 1 -> sort and take top k -> O(n log n)
+        if nums.len() == k as usize {
+            return nums;
+        }
+        let mut count_s = vec![HashSet::<i32>::new(); nums.len()];
 
-        // Solution 2 =>
+        let map = nums
+            .iter()
+            .fold(HashMap::<i32, i32>::new(), |mut acc, key| {
+                *acc.entry(*key).or_default() += 1;
+                acc
+            });
 
-        let mut map: HashMap<i32, i32> = HashMap::new();
-        let mut count_s = vec![0i32; nums.len()];
+        for (k, v) in map.iter() {
+            count_s[(*v - 1) as usize].insert(*k);
+        }
+
+        let mut res: Vec<i32> = vec![];
+
+        for i in (0..count_s.len()).rev() {
+            for x in count_s[i].iter() {
+                if res.len() == k as usize {
+                    return res;
+                } else {
+                    res.append(&mut vec![*x])
+                }
+            }
+        }
+
+        res
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,31 @@
 use std::collections::{HashMap, HashSet};
 
 pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    // Solution 1 -> sort and take top k -> O(n log n)
+    if nums.len() == k as usize {
+        return nums;
+    }
 
-    let mut map: HashMap<i32, i32> = HashMap::new();
     let mut count_s = vec![HashSet::<i32>::new(); nums.len()];
 
-    let mut top_index: usize = 0;
+    let map = nums
+        .iter()
+        .fold(HashMap::<i32, i32>::new(), |mut acc, key| {
+            *acc.entry(*key).or_default() += 1;
+            acc
+        });
 
-    for num in nums {
-        // if key exist, mut the vec!
-        let x = map.get_mut(&num);
-        match x {
-            None => {
-                map.insert(num, 0);
-                count_s[0].insert(num);
-            }
-            Some(xx) => {
-                let temp = *xx + 1;
-                if temp as usize > top_index {
-                    top_index = temp as usize;
-                }
-
-                // delete bucket set previous count
-                let pre = *xx;
-                count_s[pre as usize].remove(&num);
-
-                // update count bucket
-                count_s[temp as usize].insert(num);
-                // update map value
-                *xx = temp;
-            }
-        }
+    for (k, v) in map.iter() {
+        count_s[(*v - 1) as usize].insert(*k);
     }
 
     let mut res: Vec<i32> = vec![];
 
-    for i in (0..top_index + 1).rev() {
-        let temp: Vec<i32> = count_s[i].clone().into_iter().collect();
-
-        for x in temp {
+    for i in (0..count_s.len()).rev() {
+        for x in count_s[i].iter() {
             if res.len() == k as usize {
                 return res;
             } else {
-                res.append(&mut vec![x])
+                res.append(&mut vec![*x])
             }
         }
     }
@@ -52,5 +34,5 @@ pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
 }
 
 fn main() {
-    println!("{:?}", top_k_frequent(vec![1, 1, 1, 2, 2, 3], 2));
+    println!("{:?}", top_k_frequent(vec![-1, -1], 1));
 }
