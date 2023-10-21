533d29f916d8665dca0d1fe412f748ae080b7252
Ken Wong <kiuare310@gmail.com>
2023-10-22 04:37:11 +0800

    fix(125): palindrome

--- P125_ValidPalindrome.rs
+++ P125_ValidPalindrome.rs
@@ -1,3 +1,35 @@
 impl Solution {
-    pub fn is_palindrome(s: String) -> bool {}
+    pub fn is_palindrome(s: String) -> bool {
+        let s: Vec<char> = s
+            .to_lowercase()
+            .chars()
+            .filter(|c| c.is_alphanumeric())
+            .collect();
+
+        // two pointers
+        //
+        // if s.len() == 0 {
+        //     return true;
+        // }
+
+        // let (mut left, mut right) = (0, s.len() - 1);
+
+        // while left < right {
+        //     if s.get(left) != s.get(right) {
+        //         return false;
+        //     }
+        //     left += 1;
+        //     right -= 1;
+        // }
+
+        // functional
+
+        for (c1, c2) in s.iter().zip(s.iter().rev()) {
+            if c1 != c2 {
+                return false;
+            }
+        }
+
+        true
+    }
 }
