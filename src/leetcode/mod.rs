8e160aa6cb617a171eaaf7c3f7e1ab2606b36ecd
Ken Wong <kiuare310@gmail.com>
2023-10-23 16:14:43 +0800

    feat(399): evaluate divsion in python

--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -9,5 +9,6 @@ mod p217_contains_duplicate;
 mod p238_product_of_array_except_self;
 mod p242_valid_anagram;
 mod p347_top_kfrequent_elements;
-mod p49_group_anagram;
 mod p387_first_unique_character_in_a_string;
+mod p399_evaluate_division;
+mod p49_group_anagram;
---
+++ src/leetcode/p399_evaluate_division.rs
@@ -0,0 +1,25 @@
+// #![allow(dead_code)]
+
+// use super::Solution;
+// use std::collections::HashMap;
+
+// impl Solution {
+//     pub fn calc_equation(
+//         equations: Vec<Vec<String>>,
+//         values: Vec<f64>,
+//         queries: Vec<Vec<String>>,
+//     ) -> Vec<f64> {
+//         let mut map: HashMap<String, HashMap<String, f64>> = HashMap::new();
+//         let mut res = vec![];
+
+//         // for (i, (a, b)) in equations.iter().enumerate() {}
+
+//         res
+//     }
+// }
+
+// #[cfg(test)]
+// #[test]
+// fn main() {
+//     // assert_eq!(Solution::calc_equation(), true)
+// }
